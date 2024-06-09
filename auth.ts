bb224d9abe5e5091a3125eeb2e4a36ff646c2f8c
Blaike Bradford <blaikebradford@Blaikes-MBP.attlocal.net>
2024-06-09 18:15:13 -0500

    SUCCESS auth.ts using NextAuth callbacks, settings page displays accurate JSON.session string

--- auth.ts
+++ auth.ts
@@ -2,21 +2,33 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import { prismaDB } from "./lib/db"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { getUserById } from "./data/user"
  
 export const { handlers: {GET, POST}, auth, signIn, signOut } = NextAuth({
 
   callbacks: {
     async session({ token, session }){
+      console.log({ sessionToken : token,})
       // session token.sub is the userID of the current session object
       if(token.sub && session.user)
         {
           session.user.id = token.sub;
         }
+
+        if(token.role && session.user)
+          {
+            session.user.role = token.role;
+          }
         return session;
     }, 
 
     async jwt({ token })
     {
+      if(!token.sub) {return token;}
+      const existingUser = await getUserById(token.sub);
+      if(!existingUser){return token;}
+
+      token.role = existingUser.role;
       return token;
     }
   },
