7f7e62a9f3b189d5f8d19816c25c9e4568572801
Blaike Bradford <blaikebradford@Blaikes-MBP.attlocal.net>
2024-07-19 13:56:26 -0500

    Added Github/Google sign in methods-[SUCCESS]

--- auth.config.ts
+++ auth.config.ts
@@ -2,35 +2,46 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 /*
 Credentials - this provider is used to setup external authentication within the app (i.e. github/google credentials of user)
 The provider sends the credentials to the auth service via authorize callback
 
 */
-export default { providers: [
-    Credentials({
-        async authorize(credentials){
-            // validate fields again based on schema
-            const validatedFields = LoginSchema.safeParse(credentials);
-            
-            if(validatedFields.success)
-                {
-                    console.log("Fields are validated --->")
-                    const { email, password } = validatedFields.data;
-                    // check pw to user
-                    const user = await getUserByEmail(email);
-                    if(!user || !user.password) return null; // user could not have a pw if signed with Github or other providers
-                    
-                    // compare hashPw and PW
-                    const passwordMatch = await bcrypt.compare(password, user.password);
+export default { 
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+        Credentials({
+            async authorize(credentials){
+                // validate fields again based on schema
+                const validatedFields = LoginSchema.safeParse(credentials);
+                
+                if(validatedFields.success)
+                    {
+                        console.log("Fields are validated --->")
+                        const { email, password } = validatedFields.data;
+                        // check pw to user
+                        const user = await getUserByEmail(email);
+                        if(!user || !user.password) return null; // user could not have a pw if signed with Github or other providers
+                        
+                        // compare hashPw and PW
+                        const passwordMatch = await bcrypt.compare(password, user.password);
 
-                    if(passwordMatch) return user;
-                }
-                return null;
-        }
-    })
+                        if(passwordMatch) return user;
+                    }
+                    return null;
+            }
+        })
 ] } satisfies NextAuthConfig
 
 
